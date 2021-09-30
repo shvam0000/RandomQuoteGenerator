3bb3ba2698d7dcb71f766c8e7ba168c17ba4b2c4
Shivam Shekhar <shvam0000@gmail.com>
2021-09-30 12:26:21 +0530

    ✨ Migration to TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Page/main/main';
+
+function App() {
+  return (
+    <div className='App'>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
